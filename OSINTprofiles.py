16ead376b1246a16ff827ad15aeb239ca3973d63
bertmad3400 <skrivtilbertram@gmail.com>
2021-07-10 15:53:18 +0200

    Update path for profile storage to match other git repo

--- OSINTprofiles.py
+++ OSINTprofiles.py
@@ -8,7 +8,7 @@ from pathlib import Path
 # Function for reading all profile files and returning the content in a list if given no argument, or for returning the contents of one profile if given an argument
 def getProfiles(profileName=""):
 
-    profilePath = "./OSINTprofiles/"
+    profilePath = "./OSINTprofiles/profiles/"
 
     if profileName == "":
         # Listing all the profiles by getting the OS indepentent path to profiles folder and listing files in it, and then only choosing those files that end in a .profile
