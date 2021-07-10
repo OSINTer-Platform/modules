e57a063714264fd150048774981abba8209954d3
bertmad3400 <skrivtilbertram@gmail.com>
2021-07-10 11:04:26 +0200

    Change the folder and extensions getProfiles look for
    getProfiles will now look for a folder in the root directory named
    OSINTprofiles (matching the git repo) and will only read those files
    having the .profile extension (to avoid files like README or LICENSE)

--- OSINTprofiles.py
+++ OSINTprofiles.py
@@ -7,10 +7,13 @@ from pathlib import Path
 
 # Function for reading all profile files and returning the content in a list if given no argument, or for returning the contents of one profile if given an argument
 def getProfiles(profileName=""):
-    # Listing all the profiles by getting the OS indepentent path to profiles folder and listing files in it
-    profileFiles = os.listdir(path=Path("./profiles"))
+
+    profilePath = "./OSINTprofiles/"
 
     if profileName == "":
+        # Listing all the profiles by getting the OS indepentent path to profiles folder and listing files in it, and then only choosing those files that end in a .profile
+        profileFiles = [x for x in os.listdir(path=Path(profilePath)) if ".profile" in x]
+
         # List for holding the information from all the files, so they only have to be read one
         profiles = list()
 
@@ -18,8 +21,8 @@ def getProfiles(profileName=""):
         for profile in profileFiles:
 
             # Stripping any potential trailing or leading newlines
-            profiles.append(Path("./profiles/" + profile).read_text().strip())
+            profiles.append(Path(profilePath + profile).read_text().strip())
 
         return profiles
     else:
-        return Path("./profiles/" + profileName + ".profile").read_text().strip()
+        return Path(profilePath + profileName + ".profile").read_text().strip()
