35fa7b90095c8fd5841f25101d6f5cb4921ed5fa
bertmad3400 <skrivtilbertram@gmail.com>
2021-09-10 12:32:11 +0200

    Rewrote OSINTuser to make it OOP
    Created the User class under OSINTuser to handle webpage logins.

--- OSINTuser.py
+++ OSINTuser.py
@@ -2,56 +2,62 @@ import argon2
 
 ph = argon2.PasswordHasher()
 
-def checkIfUserExists(connection, userTableName, username):
-    with connection.cursor() as cur:
-        cur.execute("SELECT EXISTS(SELECT 1 FROM {} WHERE username = %s);".format(userTableName), (username,))
-        if cur.fetchall() == []:
-            return False
-        else:
-            cur.execute("SELECT password_hash FROM {} WHERE username = %s;".format(userTableName), (username,))
+class User():
+    def __init__(self, DBConnection, userTableName, username):
+        self.DBConnection = DBConnection
+        self.userTableName = userTableName
+        self.username = username
+
+    def checkIfUserExists(self):
+        with self.DBConnection.cursor() as cur:
+            cur.execute("SELECT EXISTS(SELECT 1 FROM {} WHERE username = %s);".format(self.userTableName), (self.username,))
             if cur.fetchall() == []:
                 return False
             else:
+                cur.execute("SELECT password_hash FROM {} WHERE username = %s;".format(self.userTableName), (self.username,))
+                if cur.fetchall() == []:
+                    return False
+                else:
+                    return True
+
+    # Set the password hash for [username]
+    def setPasswordHash(self, passwordHash):
+        if self.checkIfUserExists():
+            with self.DBConnection.cursor() as cur:
+                cur.execute("UPDATE {} SET password_hash=%s WHERE username=%s;".format(self.userTableName), (passwordHash, self.username))
+                self.DBConnection.commit()
                 return True
-
-# Set the password hash for [username]
-def setPasswordHashForOSINTerUser(connection, userTableName, username, passwordHash):
-    if checkIfUserExists(connection, userTableName, username):
-        with connection.cursor() as cur:
-            cur.execute("UPDATE {} SET password_hash=%s WHERE username=%s;".format(userTableName), (passwordHash, username))
-            connection.commit()
-            return True
-    else:
-        return False
-
-# Get the hash for the password for [username]
-def getPasswordHashForOSINTerUser(connection, userTableName, username):
-    if checkIfUserExists(connection, userTableName, username):
-        with connection.cursor() as cur:
-            cur.execute("SELECT password_hash FROM {} WHERE username=%s;".format(userTableName), (username,))
-            return cur.fetchall()[0][0]
-    else:
-        return False
-
-# Will verify that clear text [password] matches [username].
-def verifyPassword(connection, userTableName, username, password):
-    if not checkIfUserExists(connection, userTableName, username):
-        return False
-    else:
-        userHash = getPasswordHashForOSINTerUser(connection, userTableName, username)
-
-        try:
-            ph.verify(userHash, password)
-
-            if ph.check_needs_rehash(userHash):
-                setPasswordHashForOSINTerUser(username, ph.hash(password))
-            return True
-
-        except argon2.exceptions.VerifyMismatchError:
+        else:
             return False
 
+    # Get the hash for the password for [username]
+    def getPasswordHash(self):
+        if self.checkIfUserExists():
+            with self.DBConnection.cursor() as cur:
+                cur.execute("SELECT password_hash FROM {} WHERE username=%s;".format(self.userTableName), (self.username,))
+                return cur.fetchall()[0][0]
+        else:
+            return False
+
+    # Will verify that clear text [password] matches the one for the current user
+    def verifyPassword(self, password):
+        if not self.checkIfUserExists():
+            return False
+        else:
+            userHash = self.getPasswordHash()
+
+            try:
+                ph.verify(userHash, password)
+
+                if ph.check_needs_rehash(userHash):
+                    self.setPasswordHash(ph.hash(password))
+                return True
+
+            except argon2.exceptions.VerifyMismatchError:
+                return False
+
 def createUser(connection, userTableName, username, password):
-    if checkIfUserExists(connection, userTableName, username):
+    if User(connection, userTableName, username).checkIfUserExists():
         return False
     else:
         with connection.cursor() as cur:
