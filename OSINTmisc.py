b9ac1f69c4d0cf443fae7c24352d77e0164a540a
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-05 13:23:11 +0100

    Add function for loading keywords from file

--- OSINTmisc.py
+++ OSINTmisc.py
@@ -53,3 +53,23 @@ def fileSafeString(unsafeString):
     unsafeString = unsafeString.strip().replace(" ", "-")
     safeString = ''.join(c for c in unsafeString if c in allowedCharacthers)
     return safeString
+
+# The keyword file should be created like this "(keyword),(keyword),(keyword);(tag);[proximity]", where keyword are the words that are looked for withing [proximity] number of characthers of each side of the first (keyword), and if found the function "locateKeywords" from OSINTtext will return (tag). [proximity] is optional, and if not specified 30 is the default value
+def decodeKeywordsFile(filePath):
+    keywords = []
+    with open(filePath, "r") as keywordFile:
+        for line in keywordFile.readlines():
+            try:
+                keywordDetails = line.strip().split(";")
+                keywordCollection = {}
+                keywordCollection["keywords"] = keywordDetails[0].split(",")
+                keywordCollection["tag"] = keywordDetails[1]
+                if len(keywordDetails) == 3:
+                    keywordCollection["proximity"] = int(keywordDetails[2])
+                else:
+                    keywordCollection["proximity"] = 30
+            except:
+                pass
+
+            keywords.append(keywordCollection)
+    return keywords
