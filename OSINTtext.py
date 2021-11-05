117205c75d87909da93ee97d2a277c8db83eae14
bertmad3400 <skrivtilbertram@gmail.com>
2021-11-05 12:21:21 +0100

    Revise regex for urls and add one for ipv6 adresses

--- OSINTtext.py
+++ OSINTtext.py
@@ -49,15 +49,18 @@ def generateTags(clearTextList):
 
     return tagList
 
+# Function for locating interresting bits and pieces in an article like ip adresses and emails
 def locateObjectsOfInterrest(clearText):
     objects = {
-                "ip-adresses" : re.compile(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'),
+                "ipv4-adresses" : re.compile(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'),
+                "ipv6-adresses" : re.compile(r'\b(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\b'),
                 "email-adresses" : re.compile(r'\b[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+\b'),
-                "urls" : re.compile(r'\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))')
+                "urls" : re.compile(r'\b([a-zA-Z]+:\/{1,3}|www\.)\S+')
             }
     results = {}
     for objectName in objects:
 
+        # Sometimes the regex's will return a tuple of the result split up based on the groups in the regex. This will combine each of the, before reuniting them as a list
         result =  [ result if type(result) != tuple else "".join(result) for result in objects[objectName].findall(clearText) ]
 
         if result != []:
