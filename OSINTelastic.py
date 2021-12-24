ad6e08a2e325ced7d6363a24c4d5dabeda5b3dbf
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-24 11:44:16 +0100

    Automatically create elasticsearch conn when initiating elasticDB instance

--- OSINTelastic.py
+++ OSINTelastic.py
@@ -1,7 +1,9 @@
+from elasticsearch import Elasticsearch
+
 class elasticDB():
-    def __init__(self, es, indexName):
-        self.es = es
+    def __init__(self, indexName):
         self.indexName = indexName
+        self.es = Elasticsearch()
 
     # Function for taking in a list of lists of articles with the first entry of each list being the name of the profile, and then removing all the articles that already has been saved in the database
     def filterArticleURLList(es, indexName, articleURLCollection):
@@ -18,7 +20,7 @@ class elasticDB():
         return filteredArticleURLDict
 
     # Function for getting each unique profile in the DB
-    def requestProfileListFromDB(es, indexName):
+    def requestProfileListFromDB(self):
         searchQ = {"size" : 0, "aggs" : {"profileNames" : {"terms" : { "field" : "profile",  "size" : 500 }}}}
 
-        return [uniqueVal["key"] for uniqueVal in es.search(searchQ, indexName)["aggregations"]["profileNames"]["buckets"]]
+        return [uniqueVal["key"] for uniqueVal in self.es.search(searchQ, self.indexName)["aggregations"]["profileNames"]["buckets"]]
