8e13fcdf1bbfd377335279091c3b39ce33578372
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-24 09:34:30 +0100

    Simplified articles class

--- OSINTobjects.py
+++ OSINTobjects.py
@@ -3,32 +3,30 @@ from datetime import datetime, timezone
 
 @dataclass
 class Article:
-    id: str
     title: str 
     description: str
     url: str
     profile: str
-    scraped: bool
+    id: str = ""
     image_url: str = ""
     author: str = ""
-    file_path = ""
     article_contents = ""
     tags: dict[str] = field(default_factory=dict)
     inserted_at: datetime = field(default=datetime.now(timezone.utc).astimezone())
     publish_date: datetime = field(default=datetime.now(timezone.utc).astimezone())
 
     def as_dict(self):
-        return {"id" : self.id,
-                "details" : {   "title" : self.title,
-                                "description" : self.description,
-                                "url" : self.url,
-                                "image_url" : self.image_url,
-                                "author" : self.author,
-                                "publish_date" : self.publish_date.strftime("%Y/%m/%d %H:%M:%S%z")
-                            },
-                "scraped" : self.scraped,
-                "file_path" : self.file_path,
-                "contents" : self.article_contents,
-                "tags" : self.tags,
-                "inserted_at" : self.inserted_at.strftime("%Y/%m/%d %H:%M:%S%z")}
+        return { "title" : self.title,
+                 "description" : self.description,
+                 "contents" : self.article_contents,
 
+                 "url" : self.url,
+                 "image_url" : self.image_url,
+                 "author" : self.author,
+                 "profile" : self.profile,
+
+                 "publish_date" : self.publish_date,
+                 "inserted_at" : self.inserted_at,
+
+                 "tags" : self.tags
+               }
