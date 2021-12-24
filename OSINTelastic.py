ed2948a50906b73fec0fbf05c8bd3efe7208bb5c
bertmad3400 <skrivtilbertram@gmail.com>
2021-12-24 14:42:35 +0100

    Fix bugs in filter article function

--- OSINTelastic.py
+++ OSINTelastic.py
@@ -7,16 +7,16 @@ class elasticDB():
         self.es = Elasticsearch()
 
     # Function for taking in a list of lists of articles with the first entry of each list being the name of the profile, and then removing all the articles that already has been saved in the database
-    def filterArticleURLList(es, indexName, articleURLCollection):
+    def filterArticleURLList(self, articleURLCollection):
         # The final list that will be returned in the same format as the articleURLCollection list, but with the already stored articles removed
         filteredArticleURLDict = {}
 
         for profileName in articleURLCollection:
             filteredArticleURLDict[profileName] = []
-            for URL in filteredArticleURLDict:
+            for URL in articleURLCollection[profileName]:
                 # Checking if the article is already stored in the es db using the URL as that is probably not going to change and is uniqe
-                if int(es.search(index=indexName, body={'query': { "term" : {"url.keyword": {"value" : URL}}}})["hits"]["total"]["value"]) == 0:
-                    newURLList.append(URL)
+                if int(self.es.search(index=self.indexName, body={'query': { "term" : {"url": {"value" : URL}}}})["hits"]["total"]["value"]) == 0:
+                    filteredArticleURLDict[profileName].append(URL)
 
         return filteredArticleURLDict
 
