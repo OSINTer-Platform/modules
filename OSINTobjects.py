9477a403420342e1b73ae8fc81b1a5dee43e1396
bertmad3400 <skrivtilbertram@gmail.com>
2022-05-02 10:38:15 +0200

    Migrate objects from dataclass to pydantic

--- OSINTobjects.py
+++ OSINTobjects.py
@@ -1,60 +1,47 @@
-from attrs import define, field
-
+from pydantic import BaseModel, HttpUrl
+from typing import Dict, List, Union, Optional
 from datetime import datetime, timezone
 
-dateFormat = "%Y-%m-%dT%H:%M:%S%z"
-
-class base():
-    def as_dict(self):
-        object_dict = {}
-
-        for attr in dir(self):
-            attrValue = getattr(self, attr)
-            if not attr.startswith("__") and not callable(getattr(self, attr)) and attrValue:
-                if isinstance(attrValue, datetime):
-                    object_dict[attr] = attrValue.strftime(dateFormat)
-                else:
-                    object_dict[attr] = attrValue
-
-        return object_dict
-
-
-
-@define(kw_only=True)
-class Article(base):
+class Article(BaseModel):
     title: str
     description: str
-    url: str
+    url: HttpUrl
     profile: str
     source: str
     publish_date: datetime
-    id: str = ""
-    image_url: str = ""
-    author: str = ""
-    formatted_content: str = ""
-    content: str = ""
-    summary: str = ""
-    tags: dict = field(factory=dict)
-    inserted_at: datetime = field(default=datetime.now(timezone.utc))
-    saved: bool = False
-    read: bool = False
+    id: Optional[str] = None
+    image_url: Optional[HttpUrl] = None
+    author: Optional[str] = None
+    formatted_content: Optional[str] = None
+    content: Optional[str] = None
+    summary: Optional[str] = None
+    tags: Optional[Dict[str, Union[ List[str],
+                           Dict[str, Union[ List[str],
+                                            Dict[str, Union [List[str], bool] ]
+                                              ]
+                                    ]
+                             ]
+                   ]
+          ] = {}
+    inserted_at: datetime = datetime.now(timezone.utc)
+    saved: Optional[bool] = None
+    read: Optional[bool] = None
     read_times: int = 0
-    similar: list = field(factory=list)
+    similar: List[int] = None
 
-@define(kw_only=True)
-class Tweet(base):
+class Tweet(BaseModel):
     twitter_id: str
     content: str
 
-    hashtags: list = field(factory=list)
-    mentions: list = field(factory=list)
+    hashtags: List[str]
+    mentions: List[str]
 
-    author_details: dict
-    OG: dict = field(factory=dict)
+    author_details: Dict[str, str] = {}
+    OG: Dict[str, str] = {}
 
     publish_date: datetime
-    inserted_at: datetime = field(default=datetime.now(timezone.utc))
+    inserted_at: datetime = datetime.now(timezone.utc)
 
     read_times: int = 0
 
-    id: str = ""
+    id: Optional[str] = None
