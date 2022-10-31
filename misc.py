cc2b61d1610ac2a01a16d09d398a0217eeaaecc4
bertmad3400 <skrivtilbertram@gmail.com>
2022-10-31 13:37:18 +0100

    Move functions for downloading and extracting gecko to misc

--- misc.py
+++ misc.py
@@ -4,6 +4,32 @@ import re
 import os
 from pathlib import Path
 
+# For decompressing the geckodriver that comes compressed in the .tar.gz format when downloading it
+import tarfile
+import json
+import requests
+
+
+# Mozilla will have an api endpoint giving a lot of information about the latest releases for the geckodriver, from which the url for the linux 64 bit has to be extracted
+def extract_driver_url():
+    driver_details = json.loads(
+        requests.get(
+            "https://api.github.com/repos/mozilla/geckodriver/releases/latest"
+        ).text
+    )
+
+    for platform_release in driver_details["assets"]:
+        if platform_release["name"].endswith("linux64.tar.gz"):
+            return platform_release["browser_download_url"]
+
+
+# Downloading and extracting the .tar.gz file the geckodriver is stored in into the tools directory
+def download_driver(driver_url):
+    driver_contents = requests.get(driver_url, stream=True)
+    with tarfile.open(fileobj=driver_contents.raw, mode="r|gz") as driver_file:
+        driver_file.extractall(path=Path("./tools/"))
+
+
 
 def create_folder(folder_name):
     if not os.path.isdir(Path("./" + folder_name)):
